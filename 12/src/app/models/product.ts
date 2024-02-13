6c60d2b6de9379a9606b32d8318514c9f4b385ff
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-13 12:24:18 -0600

    Update product.ts

--- 12/src/app/models/product.ts
+++ 12/src/app/models/product.ts
@@ -4,12 +4,9 @@ import { ProductSubCategory } from "./productsubcategory";
 export interface Product {
     productId: number;
     productGuid: string;
-    // productCategoryOldId: number;
-    // productSubCategoryOldId: number;
     name: string;
     description?: string;
     unitPrice: number;
-    // itemsAvailable: number;
     isDeleted: boolean;
     productCategory: ProductCategory;
     productSubCategory: ProductSubCategory;
