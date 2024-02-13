1fa4920631aa81c4cba06ed84e6a1d3851c6025b
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-13 12:19:56 -0600

    Update product.service.ts

--- 12/src/app/services/product.service.ts
+++ 12/src/app/services/product.service.ts
@@ -17,35 +17,10 @@ export class ProductService {
     return this.httpClient.get<Product[]>(environment.apiUrl + '/api/v1/Products');
   }  
 
-  getAndSort(sort_by: string, sort_type: string): Observable<Product[]> {
-    var url = environment.apiUrl + `/api/v1/Products/${sort_by}/${sort_type}`;
-    console.log("url: " + url);
-    return this.httpClient.get<Product[]>(url);
-  }
-  getBySubCatAndSort(subcategoryid: number, sort_by: string, sort_type: string): Observable<Product[]> {
-    console.log("subcategoryid: " + subcategoryid);
-    var url = environment.apiUrl + `/api/v1/Products2/${subcategoryid}/${sort_by}/${sort_type}`;
-    console.log("url: " + url);
-    return this.httpClient.get<Product[]>(url);
-  }
-
   getBySubCatPaged(subcategoryid: number,page: number, itemsPerPage: number): Observable<PagedResults> {
     console.log("getBySubCatPaged.page: " + page)
     console.log("getBySubCatPaged.itemsPerPage: " + itemsPerPage)
     var url = environment.apiUrl + `/api/v1/ProductsBySubCatPaged/${subcategoryid}/${page}/${itemsPerPage}`;
-    console.log("url: " + url);
-    return this.httpClient.get<PagedResults>(url);
-  }
-
-  getProductsPaged2(page: number, itemsPerPage: number): Observable<PagedResults> {
-    var url = environment.apiUrl + `/api/v1/ProductsPaged2/${page}/${itemsPerPage}`;
-    console.log("url: " + url);
-    return this.httpClient.get<PagedResults>(url);
-  }
-  
-  getBySubCatSortAndPage(subcategoryid: number, sort_by: string, sort_type: string, page: number, itemsPerPage: number): Observable<PagedResults> {
-    console.log("subcategoryid: " + subcategoryid);
-    var url = environment.apiUrl + `/api/v1/ProductsSortAndPage/${subcategoryid}/${sort_by}/${sort_type}/${page}/${itemsPerPage}`;
     console.log("url: " + url);
     return this.httpClient.get<PagedResults>(url);
   }
@@ -55,19 +30,6 @@ export class ProductService {
     return this.httpClient.post<Product>(environment.apiUrl + '/api/v1/Product', payload);
   }
 
-   getById(id: number): Observable<Product> {
-    console.log("getById: " + id);
-    var url = environment.apiUrl + `/api/v1/Product/${id}`;
-    console.log("url: " + url);
-    return this.httpClient.get<Product>(url);
-  }
-  getBySubCategoryId(id: number): Observable<Product[]> {
-    console.log("getById: " + id);
-    var url = environment.apiUrl + `/api/v1/Products/Subcategory/${id}`;
-    console.log("url: " + url);
-    return this.httpClient.get<Product[]>(url);
-  }
-  
   update(payload: Product): Observable<Product> {
    return this.httpClient.put<Product>(
     environment.apiUrl + `/api/v1/Product/${payload.productId}`,
