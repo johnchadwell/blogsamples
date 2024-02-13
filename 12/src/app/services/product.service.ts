dc2326c32f8f27979c5f21f0adbf20f4e954cb82
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-13 12:22:25 -0600

    Update product.service.ts

--- 12/src/app/services/product.service.ts
+++ 12/src/app/services/product.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import {Product} from '../models/product';
 import { Observable } from 'rxjs';
 import {PagedResults} from '../models/responses/pagedresults';
-//import config from '../../assets/config.json';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -18,27 +17,19 @@ export class ProductService {
   }  
 
   getBySubCatPaged(subcategoryid: number,page: number, itemsPerPage: number): Observable<PagedResults> {
-    console.log("getBySubCatPaged.page: " + page)
-    console.log("getBySubCatPaged.itemsPerPage: " + itemsPerPage)
     var url = environment.apiUrl + `/api/v1/ProductsBySubCatPaged/${subcategoryid}/${page}/${itemsPerPage}`;
-    console.log("url: " + url);
     return this.httpClient.get<PagedResults>(url);
   }
 
   create(payload:Product){
-    console.log("payload: " + payload);
     return this.httpClient.post<Product>(environment.apiUrl + '/api/v1/Product', payload);
   }
 
   update(payload: Product): Observable<Product> {
-   return this.httpClient.put<Product>(
-    environment.apiUrl + `/api/v1/Product/${payload.productId}`,
-    payload
-   );
+   return this.httpClient.put<Product>(environment.apiUrl + `/api/v1/Product/${payload.productId}`, payload);
   }
 
   delete(id: number) {
-    //const headers = { 'Method': 'DELETE' };
     return this.httpClient.delete(environment.apiUrl + `/api/v1/Product/${id}`);
   }
 
