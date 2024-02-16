41a41ec3126d27106c42dd1f39c8a495e4f0c2a3
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-16 14:34:41 -0600

    Delete 12/src/app/models/productselection.ts

--- 12/src/app/models/productselection.ts
+++
@@ -1,7 +0,0 @@
-import { ProductCategory } from "./productcategory";
-import { ProductSubCategory } from "./productsubcategory";
-
-export interface ProductSelection {
-    productCategoryId: number;
-    productSubCategoryId: number;
-}
