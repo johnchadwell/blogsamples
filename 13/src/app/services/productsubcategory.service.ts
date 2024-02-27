3cb4054c27d394fe1bf99302f78fd60cba68f668
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-27 10:19:45 -0600

    Update productsubcategory.service.ts

--- 13/src/app/services/productsubcategory.service.ts
+++ 13/src/app/services/productsubcategory.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import {ProductCategory} from '../models/productcategory';
 import {ProductSubCategory} from '../models/productsubcategory';
 import { Observable } from 'rxjs';
-//import config from '../../assets/config.json';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -15,27 +14,19 @@ export class ProductSubCategoryService {
 
 
   get(): Observable<ProductSubCategory[]> {
-    console.log("environment.apiUrl: " + environment.apiUrl);
-
     return this.httpClient.get<ProductSubCategory[]>(environment.apiUrl + '/api/v1/ProductSubCategories');
   }  
 
   getByCategoryId(id: number): Observable<ProductSubCategory[]> {
-    console.log("getByCategoryId: " + id);
     return this.httpClient.get<ProductSubCategory[]>(environment.apiUrl + `/api/v1/ProductSubCategories/Category/${id}`);
   }
 
    create(payload:ProductSubCategory){
-    console.log("payload: " + payload);
     return this.httpClient.post<ProductSubCategory>(environment.apiUrl + '/api/v1/ProductSubCategory', payload);
   }
 
    getById(id: number): Observable<ProductSubCategory> {
-    console.log("getById: " + id);
-    console.log("environment.apiUrl: " + environment.apiUrl);
-
     var url = environment.apiUrl + `/api/v1/ProductSubCategory/${id}`;
-    console.log("url: " + url);
     return this.httpClient.get<ProductSubCategory>(url);
   }
    
@@ -47,7 +38,6 @@ export class ProductSubCategoryService {
   }
 
   delete(id: number) {
-    //const headers = { 'Method': 'DELETE' };
     return this.httpClient.delete(environment.apiUrl + `/api/v1/ProductSubCategory/${id}`);
   }
 }
