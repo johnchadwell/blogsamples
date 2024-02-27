d14244bb78580ef82f9518666bc072777be2a1e1
john <40181510+johnchadwell@users.noreply.github.com>
2024-02-27 11:07:46 -0600

    Update orderLineItemResponse.ts

--- 13/src/app/models/responses/orderLineItemResponse.ts
+++ 13/src/app/models/responses/orderLineItemResponse.ts
@@ -1,1 +1,18 @@
+import { OrderLineItemStatus } from "../orderLineItemStatus";
 
+export interface OrderLineItemResponse {
+
+    orderLineItemId: number;
+    orderLineItemGuid: string;
+    orderId: number;
+    orderLineItemStatus: OrderLineItemStatus,
+    productId: number;
+    productName: string;
+    quantity: number;
+    priceTotal: number,
+    createdBy: string;
+    createdOn: string;
+    modifiedBy: string;
+    modifiedOn: string;
+
+}
