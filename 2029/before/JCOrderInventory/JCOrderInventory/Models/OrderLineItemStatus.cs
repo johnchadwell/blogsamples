4587317992a506334858685e88bff70fd0e58b27
JC <office@jaychadwell.com>
2024-11-13 15:39:43 -0600

    initial commit

---
+++ 2029/before/JCOrderInventory/JCOrderInventory/Models/OrderLineItemStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace JCOrderInventory.Models
+{
+    public class OrderLineItemStatus
+    {
+        public int OrderLineItemStatusId { get; set; }
+        public string Status { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
