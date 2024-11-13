234076be294c4246a84a4107f9f29d76562709f4
JC <office@jaychadwell.com>
2024-11-13 15:55:16 -0600

    Revert "initial commit"
    This reverts commit 4587317992a506334858685e88bff70fd0e58b27.

--- 2029/before/JCOrderInventory/JCOrderInventory/Models/OrderLineItemStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JCOrderInventory.Models
-{
-    public class OrderLineItemStatus
-    {
-        public int OrderLineItemStatusId { get; set; }
-        public string Status { get; set; }
-        public bool IsDeleted { get; set; }
-    }
-}
--- 2029/before/JCOrderInventory/JCOrderInventory/Models/OrderStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JCOrderInventory.Models
-{
-    public class OrderStatus
-    {
-        public int OrderStatusId { get; set; }
-        public string Status { get; set; }
-        public bool IsDeleted { get; set; }
-    }
-}
